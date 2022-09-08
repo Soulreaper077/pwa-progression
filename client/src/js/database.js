cf37b16a47fda7aafe932b6031082ace47aa8752
Jesse <jesseponce233@gmail.com>
2022-09-07 19:10:51 -0500

    getting install button to work

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,20 +13,21 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {console.log('Put request has been made');
 const noteDb = await openDB('jate', 1);
 const tx = noteDb.transaction('jate', 'readwrite');
 const store = tx.objectStore('jate');
 const request = store.put({ id: 1, value: content });
 const result = await request; 
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {console.log('Getting the data');
 const noteDb = await openDB('jate', 1);
 const tx = noteDb.transaction('jate', 'readwrite');
 const store = tx.objectStore('jate');
 const request = store.getAll(); 
 const result = await request;
-
+};
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,19 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault(); 
+    butInstall.style.visibility = 'visible';
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    window.prompt();
+    butInstall.setAttribute('disabled', true);
+    butInstall.textContent = 'Installed!';
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('Yo!', 'appinstalled', event);
+});
